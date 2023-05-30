2543b5d55c44da18f28517990f4ed71de692a8ba
idiots-dream <703347383@qq.com>
2023-05-30 22:57:00 +0800

    创建启动类

---
+++ src/main/java/com/idiots/scaffold/ScaffoldApplication.java
@@ -0,0 +1,14 @@
+package com.idiots.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wjj70
+ */
+@SpringBootApplication
+public class ScaffoldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class,args);
+    }
+}
